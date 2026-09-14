f531034dea16871877d0a7115f2168d49240ced7
MubeenBegum-tester <mubeenbegum.tester@gmail.com>
2026-09-14 23:01:44 +0530

    learned how to extract specific element in the page from the list of arrays

--- locators1.spec.js
+++ locators1.spec.js
@@ -27,7 +27,7 @@ test("Extract multiple web elements", async ({browser})=>
     parenttagname-space-childtagname (eg: ".card-body a" here parenttagname=card-body, childtagname=a)
     **/
 
-   console.log(await page.locator(".card-body a").first().textContent());
-   console.log(await page.locator(".card-body a").nth(1).textContent());
+   console.log(await page.locator(".card-body a").first().textContent()); //it will tell that get me the first element from the array list(ex, if the page has 4 elements the first element will be n(0) then n(1) n(2) n(3), so if we ask for the first element it gives n(0))
+   console.log(await page.locator(".card-body a").nth(1).textContent()); // n(1) means it extracts second element from the array list, we can just wright first or last rest of elements we should give n(number of arraylist)
 
 });
